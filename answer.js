07ee785295cf0f4c32f03c7724bc2dee2733ecc2
NehaNaharwal-Mountblue <neha.naharwal.20.1@mountblue.tech>
2022-07-07 11:25:39 +0530

    commit the second promise drill

--- answer.js
+++ answer.js
@@ -1,97 +1,77 @@
 const books = require("./PromiseProblem");
 
 //  1 
-function signIn(username){
+function signIn(username) {
     let currDate = new Date();
     let seconds = currDate.getSeconds();
     return new Promise((resolve, reject) => {
-         if(seconds > 30){
+        if ( seconds >  30) {
             resolve(username);
-         }
-         else{
-             reject("Auth Failure : error 500");
-            }
+        }
+        else {
+            reject("Auth Failure : error 500");
+        }
 
-        });
-
-    
+    });
 }
 
 //2
 
-function getBooks(){
-    let integer = Math.random();
-    return new Promise((resolve, reject) =>{
-        if(integer*10 == 1){
+function getBooks() {
+    let integer = Math.round(Math.random())
+    return new Promise((resolve, reject) => {
+        if (integer ==  1) {
             let listOfBooks = books.reduce((acc, eachBook) => {
-                if(!acc[eachBook["name"]]){
-                    acc[eachBook["name"]] = [];
-                }
                 acc.push(eachBook["name"]);
                 return acc;
-            }, {});
-            resolve(listOfBooks);   
+            }, []);
+            //console.log(listOfBooks);
+            resolve(listOfBooks);
         }
-        else{
+        else {
             reject("No Books");
         }
     });
 
 }
 
-//3 and 5
 
-function activity(){
-    let logResult = [];
-    signIn("username").then((data => {
-        logResult.logData(('SignIn-Sucess'));
-        getBooks(books).then((data) => {
-            logResult.push(logData('getBook - success'));
-        })
-        .catch((err) => {
-            console.log(err)
-            logResult.push(logData('getBook -Faliure'));
-        })
-    }))
-    .catch((err) => {
-        console.log(err);
-        logResult.push(logData('SignIn -Faliure'));
-    })
-}
+
+
 
 //4
-function logData(activity){
-    return new Promise((resolve) =>{
+function logData(record) {
+    return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(activity)
+            resolve(record)
         }, 1000);
     })
 
 }
 
-//6 and 7
-//for Marry
-signIn("Marry").then((data)=> {
-    console.log(data);
-    getBooks(books).then((data) => {
-        console.log(data);
-    }).catch((err) => {
-        console.log(err);
-    })
-}).catch((err) => {
-    console.log(err);
-})
+//3,5,6 and 7
+function RecordOfUser(username) {
+    let userLogs = [];
+    function activity(){
+        Promise.all(userLogs)
+        .then((logs) => {
+            console.log(logs);
+        })
+    }
+    signIn(username)
+        .then((data) => {
+            userLogs.push(logData(('SignIn-Sucess')));
+            return getBooks(books)
+        })
+        .then((data) => {
+            userLogs.push(logData(('getBook - success')));
+            activity();
+        })
+        .catch((err) => {
+            userLogs.push(logData((err)));
+            activity();
+        })
+}
 
-//for Emily
-signIn("Emily").then((data)=> {
-    console.log(data);
-    getBooks(books).then((data) => {
-        console.log(data);
-    }).catch((err) => {
-        console.log(err);
-    })
-}).catch((err) => {
-    console.log(err);
-})
-
-
+RecordOfUser('Mary');
+RecordOfUser('Emily');
